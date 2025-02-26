cfec1b90c9872e9c108ca4eb9616332c5fadd734
F4LCn <naoufel.jamali@gmail.com>
2025-02-26 13:37:32 +0100

    remove time from auto save msg (who needs that lol)

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -74,7 +74,15 @@ function M.get_plugin_config()
     {
       "Pocco81/auto-save.nvim",
       config = function()
-        require("auto-save").setup()
+        require("auto-save").setup({
+          execution_message = {
+            message = function() -- message to print on save
+              return ("Auto-saved")
+            end,
+            dim = 0.18,     -- dim the color of `message`
+            cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea 
+          },
+        })
       end
     },
     -- "mg979/vim-visual-multi",
